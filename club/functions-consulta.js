7326b1315e553edf3e29f872413b719601d75e88
mkt-healthatom <fabrizio.santis@healthatom.com>
2025-01-23 11:12:51 -0300

    Update functions-consulta.js

--- club/functions-consulta.js
+++ club/functions-consulta.js
@@ -113,12 +113,10 @@ function handleError() {
   // Formateo de fecha
   function formatDate(dateString) {
     if (!dateString) return null;
-    const date = new Date(dateString);
-    const day = String(date.getDate()).padStart(2, "0");
-    const month = String(date.getMonth() + 1).padStart(2, "0"); // Los meses van de 0 a 11
-    const year = date.getFullYear();
+    const [year, month, day] = dateString.split("-");
     return `${day}/${month}/${year}`;
   }
+  
 
   // Formateo de elemento Status
   function capitalizeFirstLetter(string) {
